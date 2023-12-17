d93fa76387b6ddd46d0a212a703b48dd640b8032
HoangPhong <hoangphongazura@gmail.com>
2023-12-17 19:03:19 +0700

    update cập nhật tìm all tiêng viet không dấu

--- DAM/Controllers/BaseController.cs
+++ DAM/Controllers/BaseController.cs
@@ -26,11 +26,43 @@ namespace DAM.Controllers
         }
         protected string RemoveDiacritics(string input)
         {
-            return new string(input
-                .Normalize(NormalizationForm.FormD)
-                .ToCharArray()
-                .Where(c => CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
-                .ToArray());
+            return ConvertToVietnameseWithoutDiacritics(input);
+        }
+
+        protected string ConvertToVietnameseWithoutDiacritics(string input)
+        {
+            Dictionary<char, string> diacriticMap = new Dictionary<char, string>
+        {
+            {'à', "a"}, {'á', "a"}, {'ả', "a"}, {'ã', "a"}, {'ạ', "a"},
+            {'ă', "a"}, {'ằ', "a"}, {'ắ', "a"}, {'ẳ', "a"}, {'ẵ', "a"}, {'ặ', "a"},
+            {'â', "a"}, {'ầ', "a"}, {'ấ', "a"}, {'ẩ', "a"}, {'ẫ', "a"}, {'ậ', "a"},
+            {'è', "e"}, {'é', "e"}, {'ẻ', "e"}, {'ẽ', "e"}, {'ẹ', "e"},
+            {'ê', "e"}, {'ề', "e"}, {'ế', "e"}, {'ể', "e"}, {'ễ', "e"}, {'ệ', "e"},
+            {'ì', "i"}, {'í', "i"}, {'ỉ', "i"}, {'ĩ', "i"}, {'ị', "i"},
+            {'ò', "o"}, {'ó', "o"}, {'ỏ', "o"}, {'õ', "o"}, {'ọ', "o"},
+            {'ô', "o"}, {'ồ', "o"}, {'ố', "o"}, {'ổ', "o"}, {'ỗ', "o"}, {'ộ', "o"},
+            {'ơ', "o"}, {'ờ', "o"}, {'ớ', "o"}, {'ở', "o"}, {'ỡ', "o"}, {'ợ', "o"},
+            {'ù', "u"}, {'ú', "u"}, {'ủ', "u"}, {'ũ', "u"}, {'ụ', "u"},
+            {'ư', "u"}, {'ừ', "u"}, {'ứ', "u"}, {'ử', "u"}, {'ữ', "u"}, {'ự', "u"},
+            {'ỳ', "y"}, {'ý', "y"}, {'ỷ', "y"}, {'ỹ', "y"}, {'ỵ', "y"},
+            {'đ', "d"}, {'Đ', "D"},
+        };
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                if (diacriticMap.ContainsKey(c))
+                {
+                    result.Append(diacriticMap[c]);
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
         }
     }
 }
