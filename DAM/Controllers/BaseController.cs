409335b91df72cced1a60aa710ee803de7faf40b
HoangPhong <hoangphongazura@gmail.com>
2023-12-17 20:01:35 +0700

    update hàm xóa tv

--- DAM/Controllers/BaseController.cs
+++ DAM/Controllers/BaseController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -24,45 +25,13 @@ namespace DAM.Controllers
                 default: TempData["AlertType"] = ""; break;
             }
         }
-        protected string RemoveDiacritics(string input)
+        protected static string RemoveDiacritics(string input)
         {
-            return ConvertToVietnameseWithoutDiacritics(input);
-        }
-
-        protected string ConvertToVietnameseWithoutDiacritics(string input)
-        {
-            Dictionary<char, string> diacriticMap = new Dictionary<char, string>
-        {
-            {'à', "a"}, {'á', "a"}, {'ả', "a"}, {'ã', "a"}, {'ạ', "a"},
-            {'ă', "a"}, {'ằ', "a"}, {'ắ', "a"}, {'ẳ', "a"}, {'ẵ', "a"}, {'ặ', "a"},
-            {'â', "a"}, {'ầ', "a"}, {'ấ', "a"}, {'ẩ', "a"}, {'ẫ', "a"}, {'ậ', "a"},
-            {'è', "e"}, {'é', "e"}, {'ẻ', "e"}, {'ẽ', "e"}, {'ẹ', "e"},
-            {'ê', "e"}, {'ề', "e"}, {'ế', "e"}, {'ể', "e"}, {'ễ', "e"}, {'ệ', "e"},
-            {'ì', "i"}, {'í', "i"}, {'ỉ', "i"}, {'ĩ', "i"}, {'ị', "i"},
-            {'ò', "o"}, {'ó', "o"}, {'ỏ', "o"}, {'õ', "o"}, {'ọ', "o"},
-            {'ô', "o"}, {'ồ', "o"}, {'ố', "o"}, {'ổ', "o"}, {'ỗ', "o"}, {'ộ', "o"},
-            {'ơ', "o"}, {'ờ', "o"}, {'ớ', "o"}, {'ở', "o"}, {'ỡ', "o"}, {'ợ', "o"},
-            {'ù', "u"}, {'ú', "u"}, {'ủ', "u"}, {'ũ', "u"}, {'ụ', "u"},
-            {'ư', "u"}, {'ừ', "u"}, {'ứ', "u"}, {'ử', "u"}, {'ữ', "u"}, {'ự', "u"},
-            {'ỳ', "y"}, {'ý', "y"}, {'ỷ', "y"}, {'ỹ', "y"}, {'ỵ', "y"},
-            {'đ', "d"}, {'Đ', "D"},
-        };
-
-            StringBuilder result = new StringBuilder();
-
-            foreach (char c in input)
-            {
-                if (diacriticMap.ContainsKey(c))
-                {
-                    result.Append(diacriticMap[c]);
-                }
-                else
-                {
-                    result.Append(c);
-                }
-            }
-
-            return result.ToString();
+            string normalizedString = input.Normalize(NormalizationForm.FormD);
+            Regex regex = new Regex("[^a-zA-Z0-9 đĐ]");
+            string result = regex.Replace(normalizedString, "");
+            result = result.Replace("đ", "d").Replace("Đ", "D");
+            return result;
         }
     }
 }
